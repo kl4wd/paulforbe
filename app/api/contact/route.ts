ba925c62f0979c940f89880f3b0497752e9368ae
kl4wd <shaima.derouich@edu.ece.fr>
2026-02-13 10:24:21 +0100

    pitié marche

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -88,6 +88,7 @@ export async function POST(request: Request) {
   } catch (error) {
     console.error('Erreur API Contact:', error);
     if (error instanceof z.ZodError) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         return NextResponse.json({ success: false, error: 'Données invalides', details: (error as z.ZodError<any>).errors }, { status: 400 });
     }
     return NextResponse.json({ success: false, error: 'Erreur interne' }, { status: 500 });
